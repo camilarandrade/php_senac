5c247ba524bba120b29e25096f4a2cb679062440
CAMILA ROCHA DE ANDRADE <camila.randrade@senacsp.edu.br>
2024-03-21 19:51:46 -0300

    adicionando aula 01/ e index

---
+++ 01/teste.php
@@ -0,0 +1,42 @@
+<?php
+    echo "Olá Mundo! <BR>";
+
+    //Isso é um comentário
+    
+    # Isso também é um comentário
+
+    /*
+    Isso é um comentário de múltiplas linhas
+    Eu disse, múltiplas linhas
+    */
+
+    //Decração de variáveis
+    $nome = "João";
+    $sobrenome = "Silva";
+    $nome_completo = $nome ." ". $sobrenome;
+
+    //echo $nome;
+    //echo $sobrenome;
+    echo $nome." ".$sobrenome ."<br>";
+    echo $nome_completo . "<br>";
+
+    //Variáveis numéricas
+    $idade = 25;
+    $ano = 2024;
+
+    echo $idade . "<br>";
+    echo $ano - $idade . "<br>"; //operações matemáticas
+
+    echo 'Esse é um texto em aspas simples <br>';
+    echo "Esse é um texto em aspas duplas <br>";
+
+    //Trabalhando com aspas simples e duplas
+    echo 'Dom Pedro disse: "Independencia ou Morte!" <br>';
+
+    echo "$nome tem $idade anos de idade <br>";
+    echo '$nome tem $idade anos de idade';
+
+    //Testando o case sensitive - palavras reservadas
+    echo "Texto 1 <br>";
+    ECHO "Texto 2 <br>";
+?>
