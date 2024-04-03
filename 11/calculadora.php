fc6657c4fe7690442238c2c8da41c85415b17ccd
CAMILA ROCHA DE ANDRADE <camila.randrade@senacsp.edu.br>
2024-04-03 20:42:01 -0300

    atualização atividades encapsulamento

---
+++ 11/calculadora.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Calculadora {
+    public static function somar($a, $b) {
+        return $a + $b;
+    }
+
+    public static function multiplicar ($a, $b){
+        return $a * $b;
+    }
+}
+
+echo Calculadora::multiplicar(5,5); 
+
+?>
